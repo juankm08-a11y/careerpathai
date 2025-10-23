5b4439afa7fa791327907f46282231f5bcc01f36
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-10-23 12:56:14 -0500

    create new widgets: app_button, input_field, loading_overlay and app_header

---
+++ lib/presentation/widgets/app_button.dart
@@ -0,0 +1,55 @@
+import 'package:careerpathai/core/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final bool isLoading;
+  final bool outlined;
+
+  const AppButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.isLoading = false,
+    this.outlined = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final style = outlined
+        ? OutlinedButton.styleFrom(
+            foregroundColor: AppColors.primary,
+            side: BorderSide(color: AppColors.primary),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(12),
+            ),
+          )
+        : ElevatedButton.styleFrom(
+            backgroundColor: AppColors.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          );
+
+    final child = isLoading
+        ? const SizedBox(
+            width: 20,
+            height: 20,
+            child: CircularProgressIndicator(strokeWidth: 2),
+          )
+        : Text(text);
+
+    return outlined
+        ? OutlinedButton(
+            onPressed: isLoading ? null : onPressed,
+            style: style,
+            child: child,
+          )
+        : ElevatedButton(
+            onPressed: isLoading ? null : onPressed,
+            style: style,
+            child: child,
+          );
+  }
+}
