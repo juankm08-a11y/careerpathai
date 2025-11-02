5e1e14c82a026c7f8c9238ae157aaa8a8f1a2bb3
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-02 14:40:31 -0500

    update about_page, login_page and widget input_field

--- lib/presentation/pages/about_page.dart
+++ lib/presentation/pages/about_page.dart
@@ -12,16 +12,13 @@ class AboutPage extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
+          children: [
             Text(
-              'Sobre la Universidad Cooperativa de Colombia',
+              'about_title'.tr,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
-            Text(
-              'La UCC es una institución educativa con presencia en toda Colombia.'
-              'enfocada en formar profesionales competentes, emprendedore y con visión social.',
-            ),
+            Text('about_description'.tr, style: const TextStyle(fontSize: 18)),
           ],
         ),
       ),
