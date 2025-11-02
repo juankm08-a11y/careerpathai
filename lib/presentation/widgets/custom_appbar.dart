0ef7e14e0ec0eb563405a1f37e2ce7872ffc3d51
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-01 21:51:18 -0500

    create custom_appbar, custom_radio_button and custom_switch

---
+++ lib/presentation/widgets/custom_appbar.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final List<Widget>? actions;
+}
---
+++ lib/presentation/widgets/custom_switch.dart
@@ -0,0 +1,25 @@
+import 'package:careerpathai/core/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomSwitch extends StatelessWidget {
+  final bool value;
+  final ValueChanged<bool> onChanged;
+  final String label;
+
+  const CustomSwitch({
+    super.key,
+    required this.value,
+    required this.onChanged,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+      value: value,
+      onChanged: onChanged,
+      title: Text(label),
+      activeColor: AppColors.primary,
+    );
+  }
+}
