f360c1c222a2c7116131910795749b8121153490
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-01 21:28:57 -0500

    create custom_dialog

---
+++ lib/presentation/widgets/custom_dialog.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/app_colors.dart';
+
+class CustomDialog {
+  static Future<T?> show<T>({
+    required BuildContext context,
+    required String title,
+    required String content,
+    String okText = 'OK',
+  }) {
+    return showDialog<T>(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text(okText),
+          ),
+        ],
+      ),
+    );
+  }
+}
