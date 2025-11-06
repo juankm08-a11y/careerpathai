202a78c3f3f363aba75cb8a659db8f8352da2df4
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-05 22:49:19 -0500

    update home_page

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,5 +1,14 @@
+import 'package:careerpathai/core/constants/app_colors.dart';
 import 'package:careerpathai/core/constants/app_constants.dart';
 import 'package:careerpathai/presentation/controllers/app_controller.dart';
+import 'package:careerpathai/presentation/widgets/app_button.dart';
+import 'package:careerpathai/presentation/widgets/custom_card.dart';
+import 'package:careerpathai/presentation/widgets/info_title.dart';
+import 'package:careerpathai/presentation/widgets/profile_avatar.dart';
+import 'package:careerpathai/presentation/widgets/result_progress.dart';
+import 'package:careerpathai/presentation/widgets/section_title.dart';
+import 'package:careerpathai/presentation/widgets/skill_tag.dart';
+import 'package:careerpathai/presentation/widgets/ucc_footer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -49,12 +58,78 @@ class HomePage extends StatelessWidget {
             tooltip: 'toogle Theme',
             onPressed: controller.toogleTheme,
           ),
+          IconButton(
+            onPressed: () => Get.toNamed(AppConstants.routeProfile),
+            icon: const Icon(Icons.person_outline),
+          ),
         ],
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Get.toNamed(AppConstants.routeTest),
-          child: Text(AppConstants.startTest.tr),
+
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: controller.isDark.value
+                ? [Colors.black, Colors.grey.shade900]
+                : [AppColors.primary.withValues(alpha: 0.2), Colors.white],
+          ),
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const ProfileAvatar(initials: 'A'),
+                const SizedBox(height: 16),
+                InfoTitle(title: 'Welcome Back!', subtitle: 'Juan'),
+                const SizedBox(height: 12),
+                const SectionTitle(title: 'Quick Actions'),
+                const SizedBox(height: 10),
+                Wrap(
+                  spacing: 16,
+                  runSpacing: 16,
+                  alignment: WrapAlignment.center,
+                  children: [
+                    AppButton(
+                      text: AppConstants.startTest.tr,
+                      onPressed: () => Get.toNamed(AppConstants.routeTest),
+                    ),
+                    AppButton(
+                      text: 'Results'.tr,
+                      onPressed: () => Get.toNamed(AppConstants.routeResults),
+                    ),
+                    AppButton(
+                      text: "register".tr,
+                      onPressed: () => Get.toNamed(AppConstants.routeRegister),
+                    ),
+                    const SizedBox(height: 20),
+                    const SectionTitle(title: "Progress"),
+                    const ResultProgress(value: 8, label: "speed"),
+                    const SizedBox(height: 20),
+                    const SectionTitle(title: "Your Skills"),
+                    Wrap(
+                      spacing: 8,
+                      children: const [
+                        SkillTag(text: 'Flutter'),
+                        SkillTag(text: 'Dart'),
+                        SkillTag(text: 'Firebase'),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    const CustomCard(
+                      child: Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          "Tou can customize this to show news, highlights, or app futures.",
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    const UccFooter(text: 'Ucc Footer'),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
