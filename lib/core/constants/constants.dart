4f2574aed5eb8d21ab6caaeb0fca179e0eaa9ea4
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-05 22:12:37 -0500

    add constants routes and title's

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,13 @@
+class AppConstants {
+  static const String homeTitle = 'home';
+  static const String profileTitle = 'profile';
+  static const String aboutTitle = 'about';
+  static const String startTest = 'start_test';
+
+  static const String routeHome = '/home';
+  static const String routeProfile = '/profile';
+  static const String routeAbout = '/about';
+  static const String routeTest = '/test';
+  static const String routeResults = '/results';
+  static const String routeRegister = '/register';
+}
