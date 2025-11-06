941dd1744ebba9c7b2fbdd055dd74e018e92434a
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-05 22:36:42 -0500

    update home_page: add new IconButton-language Button

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:careerpathai/core/constants/app_constants.dart';
+import 'package:careerpathai/presentation/controllers/app_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,26 +8,48 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final AppController controller = Get.find<AppController>();
     return Scaffold(
       appBar: AppBar(
-        title: Text('home'.tr),
+        title: Text(AppConstants.homeTitle),
         actions: [
           IconButton(
             icon: const Icon(Icons.person),
-            onPressed: () => Get.toNamed('/profile'),
-            tooltip: 'profile'.tr,
+            onPressed: () => Get.toNamed(AppConstants.routeProfile),
+            tooltip: AppConstants.profileTitle.tr,
           ),
           IconButton(
             icon: const Icon(Icons.info_outline),
-            onPressed: () => Get.toNamed('/about'),
-            tooltip: 'about'.tr,
+            onPressed: () => Get.toNamed(AppConstants.routeAbout),
+            tooltip: AppConstants.aboutTitle.tr,
+          ),
+          IconButton(
+            icon: const Icon(Icons.language),
+            tooltip: 'Change Language',
+            onPressed: () {
+              Get.defaultDialog(
+                title: 'Selected Language',
+                content: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => controller.changeLanguage("en"),
+                      child: const Text('English'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => controller.changeLanguage("es"),
+                      child: const Text("Español"),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         ],
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () => Get.toNamed('/test'),
-          child: Text('start_test'.tr),
+          onPressed: () => Get.toNamed(AppConstants.routeTest),
+          child: Text(AppConstants.startTest.tr),
         ),
       ),
     );
