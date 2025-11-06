d3b87033e39564148a673b75b2155617b7b462ac
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-05 22:06:44 -0500

    Update_colors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -4,31 +4,47 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primary = Color(0xFF00723F);
   static const Color secondary = Color(0xFF009E60);
+  static const Color accent = Color(0xFF80E27E);
 
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     primaryColor: primary,
-    scaffoldBackgroundColor: Colors.white,
-    appBarTheme: AppBarTheme(
+    colorScheme: ColorScheme.light(
+      primary: primary,
+      secondary: secondary,
+      surface: Colors.white,
+    ),
+    scaffoldBackgroundColor: const Color(0xFFF9F9F9),
+    appBarTheme: const AppBarTheme(
       backgroundColor: primary,
       foregroundColor: Colors.white,
+      elevation: 2,
     ),
+    textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.black87)),
     elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(backgroundColor: primary),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+      ),
     ),
   );
 
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     primaryColor: primary,
-    scaffoldBackgroundColor: Color(0xFF121212),
-
-    appBarTheme: AppBarTheme(
-      backgroundColor: primary,
-      foregroundColor: Colors.white,
+    colorScheme: ColorScheme.dark(
+      primary: primary,
+      secondary: secondary,
+      surface: const Color(0xFF1E1E1E),
     ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(backgroundColor: primary),
+    scaffoldBackgroundColor: const Color(0xFF121212),
+    appBarTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: secondary,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
     ),
   );
 }
