ca05004cdee669df6de12f230b8d9183112dbf91
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-09 08:34:14 -0500

    delete prints in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  print("Cargando variables de entorno");
   await dotenv.load(fileName: ".env");
-  print("Variables cargadas correctamente");
 
-  print("Inicializando supabase");
   await SupabaseConfig.initialize();
-  print("Supabase inicio correctamente");
 
   Get.put(AppController(), permanent: true);
   Get.put(CareerController());
