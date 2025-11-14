f0ff2c6b378c804fb40c70f3b8a0519b4252be35
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-14 08:09:59 -0500

    add activecolor

--- lib/presentation/widgets/custom_checkbox.dart
+++ lib/presentation/widgets/custom_checkbox.dart
@@ -5,11 +5,14 @@ class CustomCheckbox extends StatelessWidget {
   final bool value;
   final ValueChanged<bool?> onChanged;
   final String label;
+  final Color? activeColor;
+
   const CustomCheckbox({
     super.key,
     required this.value,
     required this.onChanged,
     required this.label,
+    this.activeColor,
   });
 
   @override
@@ -18,7 +21,7 @@ class CustomCheckbox extends StatelessWidget {
       value: value,
       onChanged: onChanged,
       title: Text(label),
-      activeColor: AppColors.primary,
+      activeColor: activeColor ?? Theme.of(context).colorScheme.primary,
       controlAffinity: ListTileControlAffinity.leading,
     );
   }
