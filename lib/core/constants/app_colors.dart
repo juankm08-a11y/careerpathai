40fb54cca44e90adef7bd79aefa5ffbf4ddb8b8b
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-14 06:43:07 -0500

    Update: theme colors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -5,45 +5,84 @@ class AppColors {
   static const Color primary = Color(0xFF00723F);
   static const Color secondary = Color(0xFF009E60);
   static const Color accent = Color(0xFF80E27E);
+  static const Color neutralBackground = Color(0xFFF9F9F9);
+  static const darkBackground = Color(0xFF121212);
 
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     primaryColor: primary,
+    scaffoldBackgroundColor: neutralBackground,
     colorScheme: ColorScheme.light(
       primary: primary,
       secondary: secondary,
       surface: Colors.white,
+      onPrimary: Colors.white,
+      onSecondary: Colors.white,
+      onSurface: Colors.black87,
     ),
-    scaffoldBackgroundColor: const Color(0xFFF9F9F9),
     appBarTheme: const AppBarTheme(
       backgroundColor: primary,
       foregroundColor: Colors.white,
-      elevation: 2,
+      elevation: 3,
+      titleTextStyle: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
     ),
-    textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.black87)),
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(
+        fontSize: 22,
+        fontWeight: FontWeight.bold,
+        color: primary,
+      ),
+      bodyMedium: TextStyle(fontSize: 16, color: Colors.black),
+      labelLarge: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: primary,
+        foregroundColor: Colors.white,
+        textStyle: const TextStyle(fontWeight: FontWeight.bold),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(12)),
         ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+      ),
+    ),
+
+    cardTheme: CardThemeData(
+      color: Colors.white,
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(16),
       ),
     ),
   );
 
-  static ThemeData darkTheme = ThemeData(
+  static final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     primaryColor: primary,
+    scaffoldBackgroundColor: darkBackground,
     colorScheme: ColorScheme.dark(
       primary: primary,
       secondary: secondary,
       surface: const Color(0xFF1E1E1E),
+      onPrimary: Colors.white,
+      onSecondary: Colors.white,
     ),
-    scaffoldBackgroundColor: const Color(0xFF121212),
-    appBarTheme: ElevatedButtonThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: secondary,
+      foregroundColor: Colors.white,
+      elevation: 2,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: secondary,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
       ),
     ),
   );
