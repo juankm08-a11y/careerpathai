be82ae5ef0721199010f94636ca6675cbc95c59f
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-15 13:14:21 -0500

    add new fields in entity career

--- lib/domain/entities/career_entity.dart
+++ lib/domain/entities/career_entity.dart
@@ -5,11 +5,30 @@ class CareerEntity {
   final List<String> skills;
   final double score;
 
+  final double? skillsMatch;
+  final String? marketDemand;
+  final List<String>? route;
+  final List<String>? tags;
+  final String? workEnvironment;
+  final double? employability;
+  final double? avgSalary;
+  final double? trend;
+  final Map<String, dynamic>? met;
+
   CareerEntity({
     required this.id,
     required this.title,
     required this.description,
     this.skills = const [],
     this.score = 0.0,
+    this.skillsMatch,
+    this.marketDemand,
+    this.route,
+    this.tags,
+    this.workEnvironment,
+    this.employability,
+    this.avgSalary,
+    this.trend,
+    this.met,
   });
 }
