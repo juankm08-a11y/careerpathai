c3547831b43413a4e001ccdf6c83444a82e4eaa5
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-15 07:12:22 -0500

    create app_routes adn app_texts

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,16 @@
+class AppRoutes {
+  static const String home = '/home';
+  static const String profile = '/profile';
+  static const String about = '/about';
+  static const String test = '/test';
+  static const String results = '/results';
+  static const String register = '/register';
+  static const String login = '/login';
+  static const String welcome = '/welcome';
+  static const String careerListPage = '/careerlistpage';
+}
+
+class AppDescriptions {
+  static const String aboutDescription =
+      'Find more about University Cooperativa de Colombia';
+}
---
+++ lib/core/constants/app_texts.dart
@@ -0,0 +1,12 @@
+class AppTexts {
+  static const homeTitle = 'home';
+  static const profileTitle = 'profile';
+  static const aboutTitle = 'about';
+  static const startTest = 'start_test';
+  static const welcome = 'home';
+  static const register = 'register';
+  static const changeLanguage = 'Change Language';
+  static const results = 'results';
+  static const logout = 'logout';
+  static const theme = 'Change Mode';
+}
