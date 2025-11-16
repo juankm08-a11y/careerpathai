5ed8bd32f4715d9eaa8beb5a676491800f63a7f2
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-16 07:29:05 -0500

    add new route: career compare

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -9,6 +9,7 @@ class AppRoutes {
   static const String welcome = '/welcome';
   static const String careerListPage = '/careerlistpage';
   static const String careerDetailPage = 'careerDetail';
+  static const String careerComparePage = 'careerCompare';
 }
 
 class AppDescriptions {
