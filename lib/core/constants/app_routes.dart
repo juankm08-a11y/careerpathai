980fc8a122b59b5a2555599f7e832705d0da3d04
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-18 14:11:24 -0500

    repaired bug in navegation

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -8,8 +8,8 @@ class AppRoutes {
   static const String login = '/login';
   static const String welcome = '/welcome';
   static const String careerListPage = '/careerlistpage';
-  static const String careerDetailPage = 'careerDetail';
-  static const String careerComparePage = 'careerCompare';
+  static const String careerDetailPage = '/careerDetail';
+  static const String careerComparePage = '/careerCompare';
 }
 
 class AppDescriptions {
