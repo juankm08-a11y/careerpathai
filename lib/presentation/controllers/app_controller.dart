677bfb08da45675407c83f61dd15b7b8d18857df
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-18 21:23:04 -0500

    delete unused import

--- lib/presentation/controllers/app_controller.dart
+++ lib/presentation/controllers/app_controller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:path/path.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AppController extends GetxController {
