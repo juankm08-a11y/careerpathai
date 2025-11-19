f9c507b0c296dd8840fa706d8414e1910aa91ec0
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-19 09:52:32 -0500

    create classes registeren and loginen

--- lib/core/translations/en.dart
+++ lib/core/translations/en.dart
@@ -1,11 +1,27 @@
 import 'package:careerpathai/core/constants/app_texts.dart';
 
-const Map<String, String> en = {
-  RegisterTexts.title: "Register",
-  RegisterTexts.fullName: "Full Name",
-  RegisterTexts.email: "Email",
-  RegisterTexts.password: "Password",
-  RegisterTexts.acceptTerms: "I accept terms and conditions",
-  RegisterTexts.button: "Register",
-  RegisterTexts.completeFields: "Please complete all fields",
-};
+class RegisterEn {
+  static const map = {
+    RegisterTexts.title: "Register",
+    RegisterTexts.fullName: "Full Name",
+    RegisterTexts.email: "Email",
+    RegisterTexts.password: "Password",
+    RegisterTexts.acceptTerms: "I accept terms and conditions",
+    RegisterTexts.button: "Register",
+    RegisterTexts.completeFields: "Please complete all fields",
+  };
+}
+
+class LoginEn {
+  static const map = {
+    LoginTexts.title: "Sign In",
+    LoginTexts.email: "Email",
+    LoginTexts.password: "Password",
+    LoginTexts.confirmPassword: "Confirm Password",
+    LoginTexts.button: "Sign In",
+    LoginTexts.createAccount: "Create an Account",
+    LoginTexts.fillFields: "Please fill all fields",
+    LoginTexts.invalidCredentials: "Invalid Credentials",
+    LoginTexts.error: "Error"
+  };
+}
